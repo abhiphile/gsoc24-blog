b20df271d4cdd626749a60307f6c1681537f0bd2
ABHISHEK KUMAR <140839576+abhiphile@users.noreply.github.com>
2024-05-23 22:16:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/gsoc24-blog/"
 })
 // https://vitejs.dev/config/
 // export default defineConfig({
