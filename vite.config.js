cc1fb4ccbadb9b0df843073245d98bc85799e542
ABHISHEK KUMAR <140839576+abhiphile@users.noreply.github.com>
2024-05-25 22:17:16 +0530

    Resolving hosting issues.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/gsoc24-blog/"
+  base: "/gsoc24-blog",
+  plugins: [react()]
 })
 // https://vitejs.dev/config/
 // export default defineConfig({
