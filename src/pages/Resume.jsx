c40ec3f8177d58e8220d84f16ad8d5e790df4185
ABHISHEK KUMAR <140839576+abhiphile@users.noreply.github.com>
2024-05-26 14:53:02 +0530

    Resume have been added to the blog.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
-import Navbar from '../Navbar'
 import Anchors from '../assets/Anchors'
 import { Divider, Typography } from 'antd';
+import Navbar from '../Navbar'
 const { Title, Paragraph, Text, Link } = Typography;
 
 const Resume = () => {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <div className="row">
         <div className="col-md-2 col-xs-12">
           <Anchors />
         </div>
         <div className="col m-2">
           <div className="m-2 border rounded p-2">
-
+            <Title level={4}>
+              My Resume
+            </Title>
+            <iframe src="https://drive.google.com/file/d/1u3oHT-KgdS-1M_2uMCoe1IgsMHFimZxw/preview" width="800" height="900" allow="autoplay"></iframe>
+            <Paragraph>
+              Link to my <a href='https://drive.google.com/file/d/1u3oHT-KgdS-1M_2uMCoe1IgsMHFimZxw/view'>Resume</a> .
+            </Paragraph>
           </div>
         </div>
       </div>
