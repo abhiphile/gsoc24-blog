7179a734299b2eec068f81eb86063e99249f86e5
ABHISHEK KUMAR <140839576+abhiphile@users.noreply.github.com>
2024-05-26 11:51:25 +0530

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/gsoc24-blog",
+  base: "/gsoc24-blog/",
   plugins: [react()]
 })
 // https://vitejs.dev/config/
