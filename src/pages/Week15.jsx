6fa37829a3932738c2dc1a436e85c25d932342d8
Abhishek Kumar <abhishek.nitdelhi@gmail.com>
2024-09-24 16:46:15 +0530

    Fixed broken image file.

--- src/pages/Week15.jsx
+++ src/pages/Week15.jsx
@@ -106,7 +106,7 @@ useEffect(() => {
               
               <Title>Pull Requests</Title>
               <Paragraph className="par">
-                <a href="https://github.com/sympy/sympy/pull/27067" className="text-success">
+                <a href="/nichols.png" className="text-success">
                   <FaCodePullRequest /> (Open) Added nichols_plot to control plots.
                 </a>
                 <div className="gap-3">
